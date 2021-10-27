ee2fbd8f38f6aced9c389496234301ebd418a159
leochantha <leo.chanthachith1999@gmail.com>
2021-10-27 19:46:52 +0200

    added blog model, controler and router

---
+++ server/routes/blog-router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const BlogCtrl = require('../controllers/blog-ctrl')
+
+const router = express.Router()
+
+router.post('/blog', BlogCtrl.createMovie)
+router.put('/blog/:id', BlogCtrl.updateMovie)
+router.delete('/blog/:id', BlogCtrl.deleteMovie)
+router.get('/blog/:id', BlogCtrl.getMovieById)
+router.get('/blogs', BlogCtrl.getMovies)
+
+module.exports = router
